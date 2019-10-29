c02d2e7533b5e4902c253ecc1c44fea82f36c9b1
DanielBalda <d.balda@alumno.um.edu.ar>
2019-10-29 19:04:36 -0300

    ultimos cambios

--- interface.py
+++ interface.py
@@ -10,7 +10,6 @@ from api import (
     ApiNotRespond
 )
 import os
-from threading import Timer
 
 
 class EmptyVar(Exception):
@@ -32,18 +31,17 @@ class InputSymbol(Exception):
 class Interface(object):
 
     def gameDifficult(self):
-        #os.system("cls")
         self.boardSize = ''
         while self.boardSize != '4' and self.boardSize != '9':
             self.boardSize = input("\u001b[34;1mGame Difficult? 4 or 9:\033[0m ")
-            os.system("cls")
+            os.system("clear")
         print("\u001b[34;1mChosen difficulty:", self.boardSize +
               "\n#### Game Starter! ####\033[0m")
         try:
             self.api = Api(self.boardSize)
             self.sudoku = Sudoku(self.api.request())
         except ApiNotRespond:
-            os.system("cls"), print("\033[91mUps! Something went wrong. Please retry again.\033[0m"), self.gameDifficult()
+            os.system("clear"), print("\033[91mUps! Something went wrong. Please retry again.\033[0m"), self.gameDifficult()
 
     def validateNumber(self, number, posx, posy):
         symbols = '[~!@#$%^&*()/_+{}":;\']+`.$'
@@ -66,34 +64,34 @@ class Interface(object):
                 self.posy = input("\033[94m#\033[0m Place position in Y: ")
                 if self.validateNumber(self.number, self.posx, self.posy):
                     self.sudoku.putNumber(int(self.number), int(self.posx), int(self.posy))
-                    os.system("cls")
+                    os.system("clear")
                     print(self.sudoku.printBoard())
             print("\n\033[92m#### Game Over! ####\033[0m")
         except PositionFixed:
-            os.system("cls"), print("\033[91m(!)\033[0m This postion is fixed!"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m This postion is fixed!"), self.game()
 
         except NumberInRow:
-            os.system("cls"), print("\033[91m(!)\033[0m The number already exists in the Row!"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m The number already exists in the Row!"), self.game()
 
         except NumberInColumn:
-            os.system("cls"), print("\033[91m(!)\033[0m The number already exists in the Column!"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m The number already exists in the Column!"), self.game()
 
         except NumberInRegion:
-            os.system("cls"), print("\033[91m(!)\033[0m The number already exists in the Region!"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m The number already exists in the Region!"), self.game()
 
         except InvalidNumber:
-            os.system("cls"), print("\033[91m(!)\033[0m Only numbers between 1 and 9.\n    X & Y between 0 and " + str(int(self.boardSize)-1) + "."), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m Only numbers between 1 and 9.\n    X & Y between 0 and " + str(int(self.boardSize)-1) + "."), self.game()
 
         except InputChar:
-            os.system("cls"), print("\033[91m(!)\033[0m Only numbers, not letters!"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m Only numbers, not letters!"), self.game()
 
         except EmptyVar:
-            os.system("cls"), print("\033[91m(!)\033[0m No spaces allowed"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m No spaces allowed"), self.game()
 
         except InputSymbol:
-            os.system("cls"), print("\033[91m(!)\033[0m No symbol allowed"), self.game()
+            os.system("clear"), print("\033[91m(!)\033[0m No symbol allowed"), self.game()
 
     def start(self):
-        os.system("cls")
+        os.system("clear")
         self.gameDifficult()
         self.game()
